b2c8ae99063d05ea49e24fa2533e3f562848b805
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-12-09 18:38:17 +0100

    standard main

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import sys
+from main_window import StartWindow, QtWidgets
+
+
+if __name__ == "__main__":
+
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QMainWindow()
+    start_window = StartWindow(window)
+    sys.exit(app.exec_())
+
